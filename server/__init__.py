3e092ce4437fb53908ad32e81529ea660f04e570
Chris Kotfila <chris.kotfila@kitware.com>
2018-09-22 23:40:08 +0000

    Add train_model endpoint

--- server/__init__.py
+++ server/__init__.py
@@ -11,8 +11,10 @@ from girder.models.item import Item
 from girder.models.folder import Folder
 from girder.plugins.jobs.models.job import Job
 from girder.constants import AccessType
-
+from girder_worker_utils.transforms.girder_io import GirderUploadToItem
 from indian_pines_cnn_classification.create import preprocess_data as _preprocess_data
+from indian_pines_cnn_classification.train import train_model as _train_model
+from indian_pines_cnn_classification.validate import classify as _classify
 from indian_pines_cnn_classification.transforms import ItemFilesToFolder, FolderFilesToItem
 
 
@@ -24,6 +26,8 @@ class IndianPines(Resource):
         self.resourceName = 'indian_pines'
 
         self.route('POST', ('preprocess_data', ), self.preprocess_data)
+        self.route('POST', ('train_model', ), self.train_model)
+        self.route('POST', ('classify', ), self.classify)
 
     @filtermodel(model=Job)
     @access.public
@@ -51,6 +55,63 @@ class IndianPines(Resource):
             ])
 
         return async_result.job
+
+
+    @filtermodel(model=Job)
+    @access.public
+    @autoDescribeRoute(
+        Description('Preprocess Data')
+        .modelParam('data_path', '', required=True, model=Item,
+                    level=AccessType.READ, paramType='query', destName='data_path')
+        .modelParam('model_path', '', required=True, model=Item,
+                    level=AccessType.WRITE, paramType='query', destName='model_path')
+        .param('numComponents', '', required=False, dataType='int', default=30)
+        .param('windowSize', '', required=False, dataType='int', default=5)
+        .param('testRatio', '', required=False, dataType='float', default=0.25))
+    def train_model(self, data_path, model_path, numComponents, windowSize, testRatio):
+
+        async_result = _train_model.delay(
+            data_path=ItemFilesToFolder(str(data_path['_id'])),
+            model_path='/tmp/{}.h5'.format(model_path['_id']),
+            numPCAcomponents=numComponents,
+            windowSize=windowSize,
+            testRatio=testRatio,
+            girder_result_hooks=[
+                GirderUploadToItem(
+                    str(model_path['_id']), upload_kwargs={'filename': 'model.h5'})
+            ]
+        )
+
+        return async_result.job
+
+
+    @filtermodel(model=Job)
+    @access.public
+    @autoDescribeRoute(
+        Description('Preprocess Data')
+        .modelParam('data_path', '', required=True, model=Item,
+                    level=AccessType.READ, paramType='query', destName='data_path')
+        .modelParam('output_path', '', required=True, model=Item,
+                    level=AccessType.WRITE, paramType='query', destName='output_path')
+        .param('numComponents', '', required=False, dataType='int', default=30)
+        .param('windowSize', '', required=False, dataType='int', default=5)
+        .param('testRatio', '', required=False, dataType='float', default=0.25))
+    def classify(self, data_path, output_path, numComponents, windowSize, testRatio):
+        pass
+#         async_result = _preprocess_data.delay(
+#
+#             data_path=ItemFilesToFolder(data_path['_id']),
+#             output_path='/tmp/{}/'.format(output_path['_id']),
+#             numComponents=numComponents,
+#             windowSize=windowSize,
+#             testRatio=testRatio,
+#
+#             girder_result_hooks=[
+#                 FolderFilesToItem(output_path['_id'])
+#             ])
+#
+#         return async_result.job
+
 
 
 def load(info):
